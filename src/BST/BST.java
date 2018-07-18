cdba84d451ad48ba9851324e9c15efc5e6c333e5
codejser <1214045596@qq.com>
2018-07-18 15:51:56 +0800

    构建二分搜索树，并实现添加，查询，遍历等方法

---
+++ src/BST/BST.java
@@ -0,0 +1,153 @@
+package BST;
+
+/**
+ * @Author: meteor @Date: 2018/7/18 14:39
+ * //构建一个二分搜索树：
+ *      左子树的值小于根结点
+ *      右子树的值大于根结点
+ *
+ */
+public class BST<E extends Comparable<E>> {
+
+    //构造结点
+    private class Node{
+        E e;
+        Node left;
+        Node right;
+
+        public Node(E e){
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    //根结点
+    private Node root;
+    //树的大小
+    private int size;
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public BST(){
+        root = null;
+        size = 0;
+    }
+
+
+    //向树中添加新的元素：递归
+    public void add(E e){
+        root = add(root,e);
+    }
+
+    private Node add(Node node , E e){
+        //递归的终止条件
+        if(node == null){
+            size ++;
+            return new Node(e);
+        }
+
+        //把一个大问题分成若干个小问题
+        if(e.compareTo(node.e) < 0)
+            node.left = add(node.left,e);
+        else
+            node.right = add(node.right,e);
+
+        return node;
+    }
+
+    //查询数中是否存在该元素
+    public boolean contains(E e){
+        return contains(root,e);
+    }
+
+    private boolean contains(Node node ,E e){
+        //递归的终止条件
+        if(node == null){
+             return false;
+        }
+
+        //大问题划分为小的问题
+        if(e.compareTo(node.e) == 0)
+            return true;
+        else if(e.compareTo(node.e) < 0)
+            return contains(node.left,e);
+        else
+            return contains(node.right,e);
+
+    }
+
+    //前序遍历二叉搜索树：递归:所谓遍历就是打印其中的值
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    private void preOrder(Node node){
+        //递归终止的条件
+        if(node == null)
+            return;
+
+        //大的问题是：中左右
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+
+    }
+
+    //中序遍历二叉搜索树：递归:所谓遍历就是打印其中的值
+    public void cenOrder(){
+        cenOrder(root);
+    }
+
+    private void cenOrder(Node node){
+        //递归终止的条件
+        if(node == null)
+            return;
+
+        //大的问题是：左中右
+
+        cenOrder(node.left);
+        System.out.println(node.e);
+        cenOrder(node.right);
+
+    }
+
+
+    //后序遍历二叉搜索树：递归:所谓遍历就是打印其中的值
+    public void lastOrder(){
+        lastOrder(root);
+    }
+
+    private void lastOrder(Node node){
+        //递归终止的条件
+        if(node == null)
+            return;
+
+        //大的问题是：左右中
+
+        lastOrder(node.left);
+        lastOrder(node.right);
+        System.out.println(node.e);
+
+    }
+
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<Integer>();
+        int[] nums = {5,3,6,8,4,2};
+        for(int i=0;i<nums.length;i++){
+            bst.add(nums[i]);
+        }
+        //bst.preOrder();
+        //bst.cenOrder();
+        bst.lastOrder();
+    }
+
+}
