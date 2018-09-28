762e7310d7e59c83413aabed4338cbb22e355be7
codejser <1214045596@qq.com>
2018-09-28 15:49:51 +0800

    冒泡排序

--- src/Sort/BubbleSort.java
+++ src/Sort/BubbleSort.java
@@ -2,15 +2,17 @@ package Sort;
 
 /**
  * @Author: meteor @Date: 2018/7/18 18:50
- * 冒泡排序：n-1趟：每一趟需要排序相邻的元素；每一趟结束后较大值都会排到右端
- *
+ * 冒泡排序：n-1趟：每一趟比较相邻的数字，最终将数组的最大值放到左端(右端)；
+ * 外层循环控制趟数：n-1;
+ * 内层循环控制每一趟比较的次数：
+ * 每一趟都会固定一个数，在下一趟中不参与比较，所以每一趟的比较次数是n-i;i代表趟数;
  */
 public class BubbleSort {
     public static void bubbleSort(int[] nums){
         int len = nums.length;
 
         for(int i=0; i<len-1;i++){
-            for(int j=0;j<len-1;j++){
+            for(int j=0;j<len-1-i;j++){
                 if (nums[j] > nums[j+1]){
                     int temp = nums[j];
                     nums[j] = nums[j+1];
