e1de35ec2191c76a6d33ce5125e86cfe7d05fdce
codejser <1214045596@qq.com>
2018-09-28 16:15:04 +0800

    插入排序

--- src/Sort/InsertSort.java
+++ src/Sort/InsertSort.java
@@ -3,6 +3,9 @@ package Sort;
 /**
  * @Author: meteor @Date: 2018/7/18 20:06
  * 直接插入排序：默认第一个数为已排序，将未排序的数组插入到已经排序的数组当中，交换法则
+ * 以下标为1的数字开始，比较与前一个数字的大小，把小的交换到大的前面
+ * 外层循环控制着下一次插入的数字
+ * 内层循环控制着需要插入的地方
  */
 public class InsertSort {
 
@@ -11,12 +14,21 @@ public class InsertSort {
         int len = nums.length;
 
         for(int i=1;i<len;i++){
-            int j = i;
+          /*  int j = i;
             while (j>0 && nums[j] <nums[j-1]){
                 int temp = nums[j];
                 nums[j] = nums[j-1];
                 nums[j-1] = temp;
                 j--;
+            }*/
+
+            for (int j=i;j>0;j--){
+                if (nums[j]<nums[j-1]){
+                    //交换相邻的两个数
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                }
             }
         }
     }
